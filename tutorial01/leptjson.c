e2fcc8a5cbb43e8776f5d3cab5b2ee2a164c096f
LingHsvan <91541821+LingHsvan@users.noreply.github.com>
2022-07-25 13:56:13 +0800

    Update leptjson.c

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -8,6 +8,11 @@ typedef struct {
     const char* json;
 }lept_context;
 
+/**
+ * @brief 过滤空格以及控制字符
+ * 
+ * @param c JSON
+ */
 static void lept_parse_whitespace(lept_context* c) {
     const char *p = c->json;
     while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
@@ -15,6 +20,45 @@ static void lept_parse_whitespace(lept_context* c) {
     c->json = p;
 }
 
+/**
+ * @brief 解析布尔值——True
+ * 
+ * @param c JSON
+ * @param v 类型 
+ * @return int 解析状态
+ */
+static int lept_parse_true(lept_context* c, lept_value* v) {
+    EXPECT(c, 't');
+    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = LEPT_TRUE;
+    return LEPT_PARSE_OK;
+}
+
+/**
+ * @brief 解析布尔值——False
+ * 
+ * @param c JSON
+ * @param v 类型 
+ * @return int 解析状态
+ */
+static int lept_parse_false(lept_context* c, lept_value* v) {
+    EXPECT(c, 'f');
+    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 4;
+    v->type = LEPT_FALSE;
+    return LEPT_PARSE_OK;
+}
+
+/**
+ * @brief 解析空字符
+ * 
+ * @param c JSON
+ * @param v 类型 
+ * @return int 解析状态
+ */
 static int lept_parse_null(lept_context* c, lept_value* v) {
     EXPECT(c, 'n');
     if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
@@ -24,23 +68,52 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
+/**
+ * @brief 解析值
+ * 
+ * @param c JSON
+ * @param v 类型 
+ * @return int 解析状态
+ */
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
+        case 't':  return lept_parse_true(c, v);
+        case 'f':  return lept_parse_false(c, v);
         case 'n':  return lept_parse_null(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
         default:   return LEPT_PARSE_INVALID_VALUE;
     }
 }
 
+/**
+ * @brief 解析函数
+ * 
+ * @param v 类型 
+ * @param json 
+ * @return int 解析状态
+ */
 int lept_parse(lept_value* v, const char* json) {
     lept_context c;
     assert(v != NULL);
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+    int ret = lept_parse_value(&c, v);
+    if (ret == LEPT_PARSE_OK) {
+        lept_parse_whitespace(&c);
+        if (*c.json != '\0') {
+            ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+        }
+    }
+    return ret;
 }
 
+/**
+ * @brief 获取类型
+ * 
+ * @param v 类型 
+ * @return lept_type 解析类型
+ */
 lept_type lept_get_type(const lept_value* v) {
     assert(v != NULL);
     return v->type;
